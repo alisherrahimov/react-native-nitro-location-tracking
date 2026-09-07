3f3f601fefb10b8444917df25af16e15e57e0382
alisherraximov <alisher.raximov@bellissimo.uz>
2026-09-07 14:12:43 +0500

    style: format import statements and object properties for consistency

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -87,9 +87,9 @@ const config: Config = {
         {
           title: 'Docs',
           items: [
-            {label: 'Introduction', to: '/docs/intro'},
-            {label: 'Installation', to: '/docs/installation'},
-            {label: 'API Reference', to: '/docs/api/types'},
+            { label: 'Introduction', to: '/docs/intro' },
+            { label: 'Installation', to: '/docs/installation' },
+            { label: 'API Reference', to: '/docs/api/types' },
           ],
         },
         {
